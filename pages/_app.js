9214ac7400e64b2510e482a7868a96fa1ca1a064
langbart <lorenzo.longobardi@gmail.com>
2024-05-09 16:17:10 +0200

    add tracker

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,13 @@ import '../public/assets/css/modal.css'
 import "../public/assets/css/swiper-custom.css";
 import React, { useEffect, useState } from "react";
 import Preloader from '../components/elements/Preloader';
+import ReactGA from 'react-ga4';
+ReactGA.initialize('G-H9SV6BZGNC');
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
