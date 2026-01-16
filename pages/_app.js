e842a4ea9f67bd163d0cbc8a9fc66dc1f398e906
langbart <lorenzo.longobardi@gmail.com>
2026-01-16 09:31:38 +0100

    Integrate Google Analytics with updated tracking script in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,8 @@ import '../public/assets/css/style.css'
 import '../public/assets/css/modal.css'
 import "../public/assets/css/swiper-custom.css";
 import React, { useEffect, useState } from "react";
+import Script from "next/script";
 import Preloader from '../components/elements/Preloader';
-import ReactGA from 'react-ga4';
-ReactGA.initialize('G-NQ0FZ0QHVH');
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
@@ -45,6 +44,19 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-H9SV6BGNC"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-H9SV6BGNC');
+        `}
+      </Script>
       {loading ? (
         <Preloader />
       ) : (
